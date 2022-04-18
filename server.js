51d84a5342a36b87158784c61db4d06bf99182ff
theahishka <abdullafaizov@gmail.com>
2022-04-18 12:51:18 +0300

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
